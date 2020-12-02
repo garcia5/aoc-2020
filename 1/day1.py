601dfd57c0823cd23eabcbc63c50fe34f322c59b
garcia5 <garcia5@rpi.edu>
2020-12-01 20:52:02 -0500

    little better

--- 1/day1.py
+++ 1/day1.py
@@ -1,15 +1,12 @@
-# read into dict
 input_dict = {int(k): 1 for k in open('input1', 'r').read().splitlines()}
 
 for year1 in input_dict.keys():
     for year2 in input_dict.keys():
-        if year1 < year2:
+
+        if year1 + year2 > 2020:
             continue
 
-        # print(f'year1: {year1}')
-        # print(f'year2: {year2}')
-        # print(f'2020 - year1 - year2: {2020-year1-year2}')
-        # print('')
         if (2020 - year1 - year2) in input_dict.keys():
+            print(f'{year1} {year2} {2020-year1-year2}')
             print(year1 * year2 * (2020-year1-year2))
             exit()

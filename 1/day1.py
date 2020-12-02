53ec3ae8613fdb4e4251f3eea58023e2d5746517
garcia5 <garcia5@rpi.edu>
2020-12-01 20:41:28 -0500

    better day 1 with dict

--- 1/day1.py
+++ 1/day1.py
@@ -1,12 +1,15 @@
-input = open('input1', 'r').read().splitlines()
+# read into dict
+input_dict = {int(k): 1 for k in open('input1', 'r').read().splitlines()}
 
-for i, lni in enumerate(input):
-    for j, lnj in enumerate(input):
-        for k, lnk in enumerate(input):
-            lni = int(lni)
-            lnj = int(lnj)
-            lnk = int(lnk)
-            if lni + lnj + lnk == 2020:
-                res = lni * lnj * lnk
-                print(f'y1: {lni}, y2: {lnj}, y3: {lnk}, y1 * y2 * y3 = {res}')
-                exit()
+for year1 in input_dict.keys():
+    for year2 in input_dict.keys():
+        if year1 < year2:
+            continue
+
+        # print(f'year1: {year1}')
+        # print(f'year2: {year2}')
+        # print(f'2020 - year1 - year2: {2020-year1-year2}')
+        # print('')
+        if (2020 - year1 - year2) in input_dict.keys():
+            print(year1 * year2 * (2020-year1-year2))
+            exit()
